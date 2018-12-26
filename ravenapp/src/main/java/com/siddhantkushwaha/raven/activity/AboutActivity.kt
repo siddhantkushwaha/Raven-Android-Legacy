9591c53facab0d60093de39df02629b3318c6ad6
Siddhant Kushwaha <k16.siddhant@gmail.com>
2018-12-26 18:39:08 +0530

    Reformatting

---
+++ ravenapp/src/main/java/com/siddhantkushwaha/raven/activity/AboutActivity.kt
@@ -0,0 +1,14 @@
+package com.siddhantkushwaha.raven.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.siddhantkushwaha.raven.R
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R
+                .layout.activity_about)
+    }
+}
