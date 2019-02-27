94b9697e126dca4eff2f9da35e8f810ee25e76ca
Siddhant Kushwaha <k16.siddhant@gmail.com>
2019-02-27 20:50:50 +0530

    Bug fix in AboutActivity

--- ravenapp/src/main/java/com/siddhantkushwaha/raven/activity/AboutActivity.kt
+++ ravenapp/src/main/java/com/siddhantkushwaha/raven/activity/AboutActivity.kt
@@ -36,6 +36,6 @@ class AboutActivity : AppCompatActivity() {
         }
 
         versionName.text = "v${BuildConfig.VERSION_NAME}"
-        versionCode.text = "$versionCode-{BuildConfig.VERSION_CODE.toString()}"
+        versionCode.text = "versionCode-${BuildConfig.VERSION_CODE.toString()}"
     }
 }
