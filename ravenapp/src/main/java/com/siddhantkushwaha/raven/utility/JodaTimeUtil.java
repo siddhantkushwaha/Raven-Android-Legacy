505df43cafe35f8d98d16789e3d9635f64ca913a
Siddhant Kushwaha <k16.siddhant@gmail.com>
2019-05-08 21:32:00 +0530

    fixing the zone issue

--- ravenapp/src/main/java/com/siddhantkushwaha/raven/utility/JodaTimeUtil.java
+++ ravenapp/src/main/java/com/siddhantkushwaha/raven/utility/JodaTimeUtil.java
@@ -1,49 +1,60 @@
 package com.siddhantkushwaha.raven.utility;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.firebase.Timestamp;
 
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.joda.time.LocalDate;
-
-import java.util.Date;
 
 
 public class JodaTimeUtil {
 
-    public static boolean isToday(DateTime time) {
-        return LocalDate.now().compareTo(new LocalDate(time)) == 0;
+    public static boolean isToday(@NonNull DateTime time) {
+        time = getInDefaultTimeZone(time);
+        return LocalDate.now(DateTimeZone.getDefault()).compareTo(new LocalDate(time)) == 0;
     }
 
-    public static boolean isTomorrow(DateTime time) {
-        return LocalDate.now().plusDays(1).compareTo(new LocalDate(time)) == 0;
+    public static boolean isTomorrow(@NonNull DateTime time) {
+
+        time = getInDefaultTimeZone(time);
+        return LocalDate.now(DateTimeZone.getDefault()).plusDays(1).compareTo(new LocalDate(time)) == 0;
     }
 
-    public static boolean isYesterday(DateTime time) {
-        return LocalDate.now().minusDays(1).compareTo(new LocalDate(time)) == 0;
+    public static boolean isYesterday(@NonNull DateTime time) {
+
+        time = getInDefaultTimeZone(time);
+        return LocalDate.now(DateTimeZone.getDefault()).minusDays(1).compareTo(new LocalDate(time)) == 0;
     }
 
-    public static int dateCmp(DateTime dateTime1, DateTime dateTime2) {
-        return dateTime1.toLocalDate().compareTo(dateTime2.toLocalDate());
+    public static int dateCmp(@NonNull DateTime dateTime1, @NonNull DateTime dateTime2) {
+        return dateTime1.compareTo(dateTime2);
     }
 
-    public static DateTime getJodaDateTime(Date date) {
-        return new DateTime(date);
+    public static DateTime getInDefaultTimeZone(@NonNull DateTime dateTime) {
+        return dateTime.toDateTime(DateTimeZone.getDefault());
     }
 
     public static DateTime getDateTime(@Nullable Timestamp timestamp) {
 
         if (timestamp == null)
             return null;
-        return new DateTime(timestamp.toDate());
+
+        DateTime dateTime = new DateTime(timestamp.toDate());
+        return dateTime.toDateTime(DateTimeZone.getDefault());
     }
 
     public static String getDateTimeAsString(@Nullable Timestamp timestamp) {
 
-        if (timestamp == null)
+        DateTime dateTime = getDateTime(timestamp);
+
+        if (dateTime == null)
             return null;
 
-        return new DateTime(timestamp.toDate()).toString();
+        return dateTime.toString();
     }
+
+
 }
